7e055cb9faf72d5ba0ccc6036ed3e73ecfe41a1e
NooitBart <nooitbart@outlook.com>
2023-03-23 14:14:35 +0100

    added gitgnore and other setup

---
+++ Controllers/ContentWriterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentWriterService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContentWriterController : Controller
+    {
+        [HttpGet("/create")]
+        public String createData()
+        {
+            return "works";
+        }
+    }
+}
---
+++ Models/Content.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+using System.Runtime.Serialization;
+
+namespace ContentWriterService.Models
+{
+    public class Content
+    {
+        public string Text { get; set; }
+    }
+}
