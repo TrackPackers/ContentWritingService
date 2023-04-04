f59ee9f32fc664a1c3d8828efef811614ee8f963
BartVnim <65620353+BartvNim@users.noreply.github.com>
2023-04-04 14:04:26 +0200

    update kafka env
    #6

--- Messaging/KafkaController.cs
+++ Messaging/KafkaController.cs
@@ -6,9 +6,10 @@ namespace ContentWriterService.Messaging
     {
         private readonly ProducerConfig _config;
 
-        public KafkaController()
+        public KafkaController(IConfiguration configuration)
         {
-            _config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            Console.WriteLine(configuration["KAFKA_URI"]);
+            _config = new ProducerConfig { BootstrapServers = configuration["KAFKA_URI"] };
         }
 
         public async Task ProduceAsync(string topic, string message)
